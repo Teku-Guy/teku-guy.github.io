21facf37cfe603fedd0e912cd839e8f69d9bb33d
Teku-Guy <gusmuratalla@gmail.com>
2016-04-19 11:47:54 -0700

    edits

---
+++ assets/js/script.js
@@ -0,0 +1,11 @@
+jQuery(document).ready(function() {
+    
+    jQuery(".menu-trigger").click(function() {
+        
+        jQuery(".nav").slideToggle(400, function() {
+            jQuery(this).toggleClass("nav-expanded").css('display', '');
+        });
+        
+    });
+    
+});

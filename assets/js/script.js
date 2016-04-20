efea6b2be949704bb3b108f647c4140855ac854c
Teku-Guy <gusmuratalla@gmail.com>
2016-04-19 17:07:40 -0700

    Fix nav bar for mobile devices

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,7 +2,7 @@ jQuery(document).ready(function() {
     
     jQuery(".menu-trigger").click(function() {
         
-        jQuery(".nav").slideToggle(400, function() {
+        jQuery(".nav").slideToggle(350, function() {
             jQuery(this).toggleClass("nav-expanded").css('display', '');
         });
         
